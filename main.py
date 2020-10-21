ee43f84d9a161b3d8c2bb38403004050581119dc
SergonJr <sgzzsifuentes@gmail.com>
2020-10-21 18:14:48 -0500

    Added Parser

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from sly import Lexer
+from sly import Parser
 
 class BasicLexer(Lexer):
     tokens = {
+        'FUN',
         'ID',
         'INT',
         'FLOAT',
@@ -13,12 +15,15 @@ class BasicLexer(Lexer):
         'TO',
         'WHILE',
         'ARROW',
-        'COMMENT'
+        'COMMENT',
+        'STRING',
+        'EQEQ'
     }
 
     literals = { '=', '+', '-', '*', '/', '(', ')', ',', ';', '.', ' '}
 
     # Define tokens
+    FUN = r'function'
     IF = r'if'
     THEN = r'then'
     ELSE = r'else'
@@ -27,6 +32,9 @@ class BasicLexer(Lexer):
     WHILE = r'while'
     ARROW = r'->'
     ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
+    STRING = r'\".*?\"'
+
+    EQEQ = r'=='
 
     @_(r'\d+\.\d*')
     def FLOAT(self, x):
@@ -50,8 +58,100 @@ class BasicLexer(Lexer):
     def newline(self, t):
         self.lineno = t.value.count('\n')
 
+class BasicParser(Parser):
+    tokens = BasicLexer.tokens
+
+    precedence = (
+        ('left', '+', '-'),
+        ('left', '*', '/'),
+        ('right', 'UMINUS'),           
+    )
+
+    def __init__(self):
+        self.env = {}
+
+    # Statements
+
+    @_('')
+    def statement(self, x):
+        pass
+
+    @_('FOR var_assign TO expr THEN statement')
+    def statement(self, x):
+        return ('for_loop', ('for_loop_setup', x.var_assign, x.expr), x.statement)
+
+    @_('IF condition THEN statement ELSE statement')
+    def statement(self, x):
+        return ('if_stmt', x.condition, ('branch', x.statementA, x.statementB))
+
+    @_('FUN ID "(" ")" ARROW statement')
+    def statement(self, x):
+        return ('fun_def', x.ID, x.statement)
+
+    @_('ID "(" ")"')
+    def statement(self, x):
+        return ('fun_call', x.ID)
+    
+    @_('var_assign')
+    def statement(self, x):
+        return x.var_assign
+
+    @_('expr')
+    def statement(self, x):
+        return (x.expr)
+
+    # Conditions
+
+    @_('expr EQEQ expr')
+    def condition(self, x):
+        return ('condition_eqeq', x.expr0, x.expr1)
+
+    # Variable Assignment
+
+    @_('ID "=" expr')
+    def var_assign(self, x):
+        return ('var_assign', x.ID, x.expr)
+
+    @_('ID "=" STRING')
+    def var_assign(self, x):
+        return ('var_assign', x.ID, x.STRING)
+
+    @_('expr "+" expr')
+    def expr(self, x):
+        return ('add', x.expr0, x.expr1)
+
+    @_('expr "-" expr')
+    def expr(self, x):
+        return ('sub', x.expr0, x.expr1)
+
+    @_('expr "*" expr')
+    def expr(self, x):
+        return ('mul', x.expr0, x.expr1)
+
+    @_('expr "/" expr')
+    def expr(self, x):
+        return ('div', x.expr0, x.expr1)
+
+    @_('"-" expr %prec UMINUS')
+    def expr(self, x):
+        return x.expr
+
+    @_('ID')
+    def expr(self, x):
+        return ('var', x.ID)
+
+    @_('INT')
+    def expr(self, x):
+        return ('int', x.INT)
+
+    @_('FLOAT')
+    def expr(self, x):
+        return ('float', x.FLOAT)
+
+
 if __name__ == '__main__':
     lexer = BasicLexer()
+    parser = BasicParser()
     env = {}
     while True:
         try:
@@ -59,6 +159,5 @@ if __name__ == '__main__':
         except EOFError:
             break
         if text:
-            lex = lexer.tokenize(text)
-            for token in lex:
-                print(token)
+            tree = parser.parse(lexer.tokenize(text))
+            print(tree)
